f3328a94623fbc649fe55ce219c27f898b31639b
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-07-09 21:24:16 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    public_cart_items_index
+    public_cart_items_index_path
   end
   
   def after_sign_out_path_for(resource)
@@ -12,6 +12,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:telephone_number])
+    
   end
 end
