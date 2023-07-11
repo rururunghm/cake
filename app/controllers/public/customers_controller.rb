9884cf21d1df168a7bffb98864b89e726b414b90
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-07-11 12:32:52 +0000

    update

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,7 @@
 class Public::CustomersController < ApplicationController
   
   def show
-    @customer = Customer.find(params[:id])
-    @customer = current_user
+    @customer = current_customer
   end 
   
   def edit
