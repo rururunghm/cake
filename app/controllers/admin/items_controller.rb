f1f562a08a547ece327ce7a7fa834e432d2bb4e3
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-07-18 21:22:20 +0000

    update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,36 @@
 class Admin::ItemsController < ApplicationController
+  
   def index
+    @items = Item.all
+   
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+   
+    @item.save
+    redirect_to admin_items_path
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
-  def show
+  def update
   end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+   private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price, :is_active)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :customers
+  has_one_attached :image
 end
