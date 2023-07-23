49e4c3efd0957acfe50092aa2dc4043dd5d5e76d
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-07-23 13:36:59 +0000

    update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,26 @@
 class Public::CartItemsController < ApplicationController
   
   def index
-    @cart_item = CartItem.new
-    @cart_item = CartItem.all
+    @cart_item = Cartitem.new
+    @cart_items = Cartitem.all
   end
   
   def create
-    @cart_item = CartItem.new(cart_item_params)
+    @cart_item = Cartitem.new(cart_item_params)
+    @cart_item.save
+    redirect_to public_orders_path
   end
   
   def update
   end
   
   def destroy
+  end
+  
+   private
+
+  def cart_items_params
+    params.require(:cart_items).permit(:amount)
   end
   
 end
--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,2 +1,28 @@
 class Public::DeliveriesController < ApplicationController
+  def index
+    @delivery = Delivery.new
+    @deliveries = Derivery.all
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+  end
+  
+   private
+
+  def deriveries_params
+    params.require(:deliveies).permit(:gip_code, :address, :address_name)
+  end
+  
 end
