07fcc4ca526224e0d36983665ca033a73e16e1e5
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-07-29 12:10:47 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,5 @@
 Rails.application.routes.draw do
  
- 
-  namespace :admin do
-    get 'homes/top'
-    resources :order_details,only: [:update]
-    resources :orders,only: [:update, :show]
-    resources :customers,only: [:index, :update, :show, :create, :edit]
-    resources :items,only: [:index, :new, :show, :update, :create, :edit]
-  end
- 
-  namespace :public do
-    resources :deliveries,only: [:index, :edit, :create, :update, :destroy]
-    resources :orders,only: [:index, :show, :new, :create]
-     
-    resources :items,only: [:index, :show]
-    resources :cart_items,only: [:index, :update, :destroy, :create]
-    resources :customers, only: [:edit, :update, :show]
-    
-  end
-  
-  get "/items" => "public/items#index"
-  get "/cart_items" => "public/cart_items#index"
-  get "/customers/information/edit" => "public/customers#edit"
-  get "/customers/mypage" => "public/customers#show"
-  get "/customers/complete" => "public/customers#complete"
-  get "/about" => "public/homes#about"
-  
-  
-  get 'admin/items/:id/edit' => 'admin/items#edit'
-  root to: "public/homes#top"
-  
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -39,6 +9,43 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
+  get 'admin/items/:id/edit' => 'admin/items#edit'
+  root to: "public/homes#top"
+  
+ 
+  namespace :admin do
+    get 'homes/top'
+    resources :order_details,only: [:update]
+    resources :orders,only: [:update, :show]
+    resources :customers,only: [:index, :update, :show, :create, :edit]
+    resources :items,only: [:index, :new, :show, :update, :create, :edit]
+  end
+ 
+  scope module: :public do
+    resources :deliveries,only: [:index, :edit, :create, :update, :destroy]
+    resources :orders,only: [:index, :show, :new, :create] do
+      collection do
+        post :confirm
+        get :complete
+      end 
+    end  
+    resources :items,only: [:index, :show]
+    resources :cart_items,only: [:index, :update, :destroy, :create]
+    resources :customers, only: [:edit, :update, :show]
+    
+    get "/items" => "public/items#index"
+    get "/cart_items" => "public/cart_items#index"
+    get "/customers/information/edit" => "public/customers#edit"
+    get "/customers/mypage" => "public/customers#show"
+    get "/customers/complete" => "public/customers#complete"
+    get "/about" => "public/homes#about"
+  
+  end
+  
+  
+  
+  
+ 
 
 
 
