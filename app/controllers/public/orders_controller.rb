19997a074ba90f291314f24642249806a544ea3e
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-08-01 13:43:13 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -29,19 +29,38 @@ class Public::OrdersController < ApplicationController
     end
       @order.payment_methods = order_params[:payment_methods]
       @cart_items = current_customer.cart_items
+        @total = 0
+        @cart_items.each do |cart_item| 
+          @total = ((cart_item.item.price * 1.1).floor) * cart_item.amount + @total
+        end
   end
   
   def complete
+    
   end
   
   def create
-    
-    @order = Order.new(order_params)
-    @order = current_customer
-    @order.customer_id = current_customer.id
-    
-    @order.save
-    redirect_to  confirm_orders_path
+       @order = Order.new
+    if order_params[:delivery_method] == '0'
+      @order.gip_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.address_name = current_customer.first_name + current_customer.last_name
+    elsif order_params[:delivery_method] == '1'
+      delivery = Delivery.find(order_params[:select_address])
+      @order.gip_code = delivery.gip_code
+      @order.address = delivery.address
+      @order.address_name = delivery.address_name
+    else
+      @order.gip_code = order_params[:gip_code]
+      @order.address = order_params[:address]
+      @order.address_name = order_params[:address_name]
+    end
+      @order.payment_methods = order_params[:payment_methods]
+      @cart_items = current_customer.cart_items
+       @total = 0
+        @cart_items.each do |cart_item| 
+          @total = ((cart_item.item.price * 1.1).floor) * cart_item.amount + @total
+        end
   end
   
   def show
