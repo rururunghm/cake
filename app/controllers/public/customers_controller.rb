1a3b396bbb2e71645de77b0860d980b536829e4b
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-08-06 05:38:43 +0000

    finish

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,6 +20,7 @@ class Public::CustomersController < ApplicationController
   end
   
   def secession
+    @customer = current_customer
     @customer.update(is_deleted: true)
     reset_session
     redirect_to new_customer_registration_path
